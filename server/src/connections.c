52f669215ff98528772c25fd5b59330679e08b9a
niborg <robin.caboche@epitech.eu>
2024-05-31 18:25:13 +0200

    feat(clean CS): clean code

--- server/src/connections.c
+++ server/src/connections.c
@@ -8,50 +8,50 @@
 #include "../include/main.h"
 
 // Add the child sockets to the readfds set
-void add_child_socket(server_t *s, int sd, int max_sd)
+void add_child_socket(server_t *s, int *sd, int *max_sd)
 {
     for (int i = 0; i < s->max_client_team; i++) {
-        sd = s->client_socket[i];
-        if (sd > 0)
-            FD_SET(sd, &s->readfds);
-        if (sd > max_sd)
-            max_sd = sd;
+        (*sd) = s->client_socket[i];
+        if (*sd > 0)
+            FD_SET(*sd, &s->readfds);
+        if (*sd > *max_sd)
+            *max_sd = *sd;
     }
 }
 
 // Handle the client messages
-void client_handler(server_t *s, int sd, int i)
+void client_handler(server_t *s, int *sd, int i)
 {
     char buffer[1024];
     int valread = 0;
 
-    if (FD_ISSET(sd, &s->readfds)) {
-        valread = read(sd, buffer, 1024);
+    if (FD_ISSET(*sd, &s->readfds)) {
+        valread = read(*sd, buffer, 1024);
         if (valread == 0) {
-            disconnect_client(s, sd);
+            disconnect_client(s, *sd);
             s->client_socket[i] = 0;
         } else {
             buffer[valread] = '\0';
-            send(sd, buffer, strlen(buffer), 0);
-            printf("[%d] - %s\n", sd, buffer);
+            send(*sd, buffer, strlen(buffer), 0);
+            printf("[%d] - %s\n", *sd, buffer);
         }
     }
 }
 
 // Main loop of the server
-int listener_loop(server_t *s, int sd, int max_sd)
+int listener_loop(server_t *s, int* sd, int *max_sd)
 {
     FD_ZERO(&s->readfds);
     FD_SET(s->master_socket, &s->readfds);
-    max_sd = s->master_socket;
+    *max_sd = s->master_socket;
     add_child_socket(s, sd, max_sd);
-    if ((select(max_sd + 1, &s->readfds, NULL, NULL, NULL) < 0)
+    if ((select(*max_sd + 1, &s->readfds, NULL, NULL, NULL) < 0)
         && (errno != EINTR))
         printf("Select error");
     if (FD_ISSET(s->master_socket, &s->readfds))
         accept_client(s);
     for (int i = 0; i < s->max_client_team; i++) {
-        sd = s->client_socket[i];
+        (*sd) = s->client_socket[i];
         client_handler(s, sd, i);
     }
     return 0;
@@ -67,7 +67,7 @@ int start_litener(server_t *s)
         return 84;
     printf("Listening on port %d...\n", s->port);
     while (true) {
-        if (listener_loop(s, sd, max_sd) != 0)
+        if (listener_loop(s, &sd, &max_sd) != 0)
             return 84;
     }
     return 0;
--- server/src/main.c
+++ server/src/main.c
@@ -26,14 +26,6 @@ int main(int argc, char *argv[])
         server.height <= 0 || server.freq <= 0 || server.max_client_team <= 0
         || server.team_nb <= 0)
         return help(argv[0], 84);
-    printf("port: %d\n", server.port);
-    printf("width: %d\n", server.width);
-    printf("height: %d\n", server.height);
-    printf("freq: %d\n", server.freq);
-    printf("max_client: %d\n", server.max_client_team);
-    printf("team_nb: %d\n", server.team_nb);
-    for (int i = 0; server.team_names && server.team_names[i]; i++)
-        printf("team_names[%d]: %s\n", i, server.team_names[i]);
     if (start_litener(&server) != 0)
         return destroy_server(&server, 84);
     return destroy_server(&server, 0);
