44ef5e2ee3598940b3e654424dea9d6f196f5780
Victor <victor.briez@epitech.eu>
2024-06-14 19:17:48 +0200

    [~] Refactor eject and command_eject functions to use switch statements for better readability and maintainability

--- server/src/commands/eject.c
+++ server/src/commands/eject.c
@@ -13,14 +13,20 @@ void eject_players(server_t *s, client_t *client, int x, int y)
     int x2 = x;
     int y2 = y;
 
-    if (client->orientation == NORTH)
-        y2 = ((y - 1) < 0) ? s->height - 1 : y - 1;
-    if (client->orientation == SOUTH)
-        y2 = ((y + 1) >= s->height) ? 0 : y + 1;
-    if (client->orientation == EAST)
-        x2 = ((x + 1) >= s->width) ? 0 : x + 1;
-    if (client->orientation == WEST)
-        x2 = ((x - 1) < 0) ? s->width - 1 : x - 1;
+    switch (client->orientation) {
+        case NORTH:
+            y2 = ((y - 1) < 0) ? s->height - 1 : y - 1;
+            break;
+        case SOUTH:
+            y2 = ((y + 1) >= s->height) ? 0 : y + 1;
+            break;
+        case EAST:
+            x2 = ((x + 1) >= s->width) ? 0 : x + 1;
+            break;
+        case WEST:
+            x2 = ((x - 1) < 0) ? s->width - 1 : x - 1;
+            break;
+    }
     for (int i = 0; i < MAX_CLIENTS && s->map[y][x].players[i]; i++)
         move_player(s, s->map[y][x].players[i], x2, y2);
 }
@@ -31,14 +37,20 @@ int command_eject(server_t *s, client_t *client, char *arg)
     int x = client->x;
     int y = client->y;
 
-    if (client->orientation == NORTH)
-        y = ((y - 1) < 0) ? s->height - 1 : y - 1;
-    if (client->orientation == SOUTH)
-        y = ((y + 1) >= s->height) ? 0 : y + 1;
-    if (client->orientation == EAST)
-        x = ((x + 1) >= s->width) ? 0 : x + 1;
-    if (client->orientation == WEST)
-        x = ((x - 1) < 0) ? s->width - 1 : x - 1;
+    switch (client->orientation) {
+        case NORTH:
+            y = ((y - 1) < 0) ? s->height - 1 : y - 1;
+            break;
+        case SOUTH:
+            y = ((y + 1) >= s->height) ? 0 : y + 1;
+            break;
+        case EAST:
+            x = ((x + 1) >= s->width) ? 0 : x + 1;
+            break;
+        case WEST:
+            x = ((x - 1) < 0) ? s->width - 1 : x - 1;
+            break;
+    }
     eject_players(s, client, x, y);
     destroy_eggs_from_tiles(&s->map[y][x]);
     return 0;
