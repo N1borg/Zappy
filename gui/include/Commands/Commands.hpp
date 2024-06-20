1f02143d5e5fc551e24b7064909b92964b3a24da
Valentine Tanon <valentine.pellissier-tanon@epitech.eu>
2024-06-20 11:43:58 +0200

    Maquette fonction

--- gui/include/Commands/Commands.hpp
+++ gui/include/Commands/Commands.hpp
@@ -18,6 +18,27 @@ class Commands {
         void getMapSize(const std::string& params);
         void getMapContent(const std::string& params);
         void getPlayerPosition(const std::string& params);
+        void getTileContent(const std::string& params);
+        void getTeamName(const std::string& params);
+        void getConnectionNewPlayer(const std::string& params);
+        void getPlayerLevel(const std::string& params);
+        void getPlayerInventory(const std::string& params);
+        void getPlayerExpulsion(const std::string& params);
+        void getStartofIncantation(const std::string& params);
+        void getEndofIncantation(const std::string& params);
+        void getLayingEggbyPlayer(const std::string& params);
+        void getResourceDropping(const std::string& params);
+        void getResourceCollecting(const std::string& params);
+        void getPlayerDeath(const std::string& params);
+        void getLaidEggbyPlayer(const std::string& params);
+        void getPlayerConnectioninEgg(const std::string& params);
+        void getEggDeath(const std::string& params);
+        void getTimeUnitRequest(const std::string& params);
+        void getTimeUnitModification(const std::string& params);
+        void getEndofGame(const std::string& params);
+        void getServerMessage(const std::string& params);
+        void getUnknownCommand(const std::string& params);
+        void getCommandParameter(const std::string& params);
 };
 
 #endif /* !COMMANDS_HPP_ */
--- gui/src/Commands/Commands.cpp
+++ gui/src/Commands/Commands.cpp
@@ -21,3 +21,108 @@ void Commands::getPlayerPosition(const std::string& params)
 {
     std::cout  << "Player Position: " << params << std::endl;
 }
+
+void Commands::getTileContent(const std::string& params)
+{
+    std::cout  << "Tile Content: " << params << std::endl;
+}
+
+void Commands::getTeamName(const std::string& params)
+{
+    std::cout  << "Team Name: " << params << std::endl;
+}
+
+void Commands::getConnectionNewPlayer(const std::string& params)
+{
+    std::cout  << "New Player Connection: " << params << std::endl;
+}
+
+void Commands::getPlayerLevel(const std::string& params)
+{
+    std::cout  << "Player Level: " << params << std::endl;
+}
+
+void Commands::getPlayerInventory(const std::string& params)
+{
+    std::cout  << "Player Inventory: " << params << std::endl;
+}
+
+void Commands::getPlayerExpulsion(const std::string& params)
+{
+    std::cout  << "Player Expulsion: " << params << std::endl;
+}
+
+void Commands::getStartofIncantation(const std::string& params)
+{
+    std::cout  << "Start of Incantation: " << params << std::endl;
+}
+
+void Commands::getEndofIncantation(const std::string& params)
+{
+    std::cout  << "End of Incantation: " << params << std::endl;
+}
+
+void Commands::getLayingEggbyPlayer(const std::string& params)
+{
+    std::cout  << "Laying Egg by Player: " << params << std::endl;
+}
+
+void Commands::getResourceDropping(const std::string& params)
+{
+    std::cout  << "Resource Dropping: " << params << std::endl;
+}
+
+void Commands::getResourceCollecting(const std::string& params)
+{
+    std::cout  << "Resource Collecting: " << params << std::endl;
+}
+
+void Commands::getPlayerDeath(const std::string& params)
+{
+    std::cout  << "Player Death: " << params << std::endl;
+}
+
+void Commands::getLaidEggbyPlayer(const std::string& params)
+{
+    std::cout  << "Egg Laid by Player: " << params << std::endl;
+}
+
+void Commands::getPlayerConnectioninEgg(const std::string& params)
+{
+    std::cout  << "Player Connection in Egg: " << params << std::endl;
+}
+
+void Commands::getEggDeath(const std::string& params)
+{
+    std::cout  << "Death of Egg: " << params << std::endl;
+}
+
+void Commands::getTimeUnitRequest(const std::string& params)
+{
+    std::cout  << "Time Unit Request: " << params << std::endl;
+}
+
+void Commands::getTimeUnitModification(const std::string& params)
+{
+    std::cout  << "Time Unit Modification: " << params << std::endl;
+}
+
+void Commands::getEndofGame(const std::string& params)
+{
+    std::cout  << "End of Game: " << params << std::endl;
+}
+
+void Commands::getServerMessage(const std::string& params)
+{
+    std::cout  << "Server Message: " << params << std::endl;
+}
+
+void Commands::getUnknownCommand(const std::string& params)
+{
+    std::cout  << "Unknown Command: " << params << std::endl;
+}
+
+void Commands::getCommandParameter(const std::string& params)
+{
+    std::cout  << "Command Parameter: " << params << std::endl;
+}
