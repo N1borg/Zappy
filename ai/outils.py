0512d639ea37811b6bc9d7b46566d23306b9d76d
Ananasinator <anastasia.romashka@gmail.com>
2024-06-23 16:04:47 +0200

    fix: made socket operations safer in send_message()

--- ai/outils.py
+++ ai/outils.py
@@ -4,8 +4,11 @@
 # Send a given string followed by a newline to the server.
 def send_message(sock, message):
     message_with_newline = message + '\n'
-    sock.sendall(message_with_newline.encode())
-    print(f'[->]    Sent message: {message}')
+    if sock:
+        sock.sendall(message_with_newline.encode())
+        print(f'[->]    Sent message: {message}')
+    else:
+        print("Error with socket")
 
 
 # Receive a response from the server and throw an error if the response is 'ko\n'.
